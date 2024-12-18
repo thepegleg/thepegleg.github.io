3b2803e15d2880c65af36229bff73b764ce9e81c
sebastian-92 <140918855+sebastian-92@users.noreply.github.com>
2024-12-18 14:10:13 -0500

    Proxy test

---
+++ asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/static/uv/uv.config.js
@@ -0,0 +1,9 @@
+self.__uv$config = {
+    bare:'https://tomp.app',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
