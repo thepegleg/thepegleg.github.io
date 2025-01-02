be1ef84252e903b80006e31a34300819a1fa62b0
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2025-01-02 17:52:49 -0500

    ooh private bare server

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://tomp.app',
+    bare:'https://bare.cbass92.org',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
