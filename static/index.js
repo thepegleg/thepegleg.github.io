153d705da620b76c3830c27fea5c8a194935965e
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2025-01-28 17:19:43 -0500

    skibidi skibidi hawk tuah hawk

--- static/index.js
+++ static/index.js
@@ -22,15 +22,6 @@ const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
