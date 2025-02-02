9281a5874cf2a0c6c377da74520869e589a8c99a
sebastian-92 <140918855+sebastian-92@users.noreply.github.com>
2025-02-01 21:22:09 -0500

    its too much of a pain in the butt

---
+++ asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/stream/series.js
@@ -0,0 +1,38 @@
+let params = new URLSearchParams(window.location.search);
+let movid = params.get("id");
+let streamurl;
+if (movid) {
+  var season = prompt("season?");
+  var episode = prompt("episode?");
+  fetch(
+    "https://vidapi.cbass92.org/vidsrc/" +
+      movid +
+      "?s=" +
+      season +
+      "&e=" +
+      episode
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const validsources = [];
+      data.sources.forEach((source) => {
+        console.log(source);
+        if (source.isM3U8) {
+          validsources.push(source);
+        }
+      });
+      if (validsources.length > 0) {
+        streamurl = validsources[0].url;
+        const embed = document.createElement("embed");
+        embed.frameborder = "0";
+        embed.src = "player/index.html#" + streamurl;
+        document.body.appendChild(embed);
+      } else {
+        console.log("No valid sources found. :(");
+        document.write("No valid sources found. :(");
+      }
+    });
+} else {
+  document.write("No series ID .'-'.");
+}
